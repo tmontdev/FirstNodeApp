1ad99bbe64b7c610133f149c2f59a22acfef9a1a
Thales Monteiro <the.monteiro.dev@gmail.com>
2018-06-21 00:38:59 -0300

    indo dormir, amanhã vejo como retornar os erros de autenticação

--- application/app.js
+++ application/app.js
@@ -7,22 +7,21 @@ var passportHttp = require('passport-http');
 var bodyParser = require('body-parser'); // importa o body parser, para ler as credenciais dos body request
 var cookieParser = require('cookie-parser');// importa o cookie parser para armazenar o ID de Sessão no navegador
 var expressSession = require('express-session');// importa o express session para armazenar o Id de Sessão no serverSide, entre outros paranauê's'
-const {Pool} = require('pg')
-const connectionString = 'postgresql://postgres:toor@localhost:5432/postgres'
-
-const pool = new Pool({
-  connectionString: connectionString,
-});
-
-pool.query('SELECT * from teste where username = \'tmontdv\' and password = \'Blusterysnail360\' ', (err, res) => {
-  res.rows.length
-  res.rows.forEach(row => {
-    console.log("user"+row.username)
-  });
-  pool.end()
-});
+var db = require('db.js');
+authCallback =function(err,res){
+  if (err) {
+    // error handling code goes here
+    console.log("ERROR : ",res);
+  } else {
+    // code to execute on data retrieval
+    console.log("result from db is : ",res);
+  }
+}
+var flash = require('connect-flash');
+app.use(flash());
 
 app.set('view engine', 'ejs');//define o viewEngine (Embeed JS)
+app.use(express.static('public'));
 
 //estes procedimentos precisam ser feitos antes do passport.initialize e session serem construidos
 app.use(bodyParser.urlencoded({ extender: false}));
@@ -33,46 +32,43 @@ app.use(expressSession({
   saveuninitialized: false
 }));
 
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new passportLocal.Strategy(function(username, password, done){
-
-  if(username === password){
-    done(null, { id: username, name: username});
-  }else{
-    done(null, null);
-  }
-}));
+passport.use(new passportLocal.Strategy(db.userPassAuth));
 
 passport.serializeUser(function(user, done){
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done){
-  done(null, {id: id, name: id});
+  db.findUserById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 app.get('/', function(req, res){
-    res.render('index',{
-        isAuthenticated: req.isAuthenticated(),
-        user: req.user
-    });
+  res.render('index',{
+    isAuthenticated: req.isAuthenticated(),
+    user: req.user
+  });
 });
 
 app.get('/login', function(req, res){
-    res.render('login');
+  res.render('login');
 });
 
-app.post('/login', passport.authenticate('local'), function(re, res){
+app.post('/login', passport.authenticate('local', { successRedirect: '/',
+failureRedirect: '/login',
+failureFlash: true }), function(req, res){
   res.redirect('/');
 });
 
+app.listen(port, function(){
+  console.log('http://127.0.01:' + port +'/');
+});
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
-})
-
-app.listen(port, function(){
-    console.log('http://127.0.01:' + port +'/');
 });
